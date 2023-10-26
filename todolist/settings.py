173aafe021fc67897fd9fc9d533fdaf7e12f6929
Sora <sky.roofcommon.0314@gmail.com>
2023-10-27 05:22:33 +0900

    change_setting.py(debug True to False)

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z$(01c=o!j3f_ebp1hb$%se-3=6ur0_3ww@lgkf42ov#mk-+wu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
