65ed16127b27dcf51f1ddb1425b078e6973e86e4
Sora <91284120+sorapanel@users.noreply.github.com>
2023-11-13 01:58:47 +0900

    Update forms.py
    コメント追加

--- myTask/forms.py
+++ myTask/forms.py
@@ -2,14 +2,16 @@ from django import forms
 from django.utils import timezone
 from .models import TaskModel
 
+#タスク登録用
 class TaskForm(forms.Form):
-    name = forms.CharField(label='タスク名', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    description = forms.CharField(label='詳細', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    start_date = forms.DateField(label='開始日付', input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'min':timezone.localdate(),}))
-    start_time = forms.TimeField(label='開始時間', input_formats=['%H:%M'], widget=forms.TimeInput(attrs={'class': 'form-control', 'type': 'time', 'min':timezone.localtime(),}))
-    finish_date = forms.DateField(label='終了日付', input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'min':timezone.localdate(),}))
-    finish_time = forms.TimeField(label='終了時間', input_formats=['%H:%M'], widget=forms.TimeInput(attrs={'class': 'form-control', 'type': 'time', 'min':timezone.localtime(),}))
+    name = forms.CharField(label='タスク名', widget=forms.TextInput(attrs={'class': 'form-control'}))#タスク名
+    description = forms.CharField(label='詳細', widget=forms.TextInput(attrs={'class': 'form-control'}))#詳細
+    start_date = forms.DateField(label='開始日付', input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'min':timezone.localdate(),}))#開始日付
+    start_time = forms.TimeField(label='開始時間', input_formats=['%H:%M'], widget=forms.TimeInput(attrs={'class': 'form-control', 'type': 'time', 'min':timezone.localtime(),}))#開始時間
+    finish_date = forms.DateField(label='終了日付', input_formats=['%Y-%m-%d'], widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'min':timezone.localdate(),}))#終了日付
+    finish_time = forms.TimeField(label='終了時間', input_formats=['%H:%M'], widget=forms.TimeInput(attrs={'class': 'form-control', 'type': 'time', 'min':timezone.localtime(),}))#終了時間
 
+#タスク更新用
 class TaskForm_Update(forms.ModelForm):
     class Meta:
         model = TaskModel
